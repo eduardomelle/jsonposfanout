dde10aebd6e94df144bc4c27e5d12f88388ae1f2
Eduardo Orlandi Melle <eduardomelle@gmail.com>
2024-12-24 08:20:22 -0300

    first commit

---
+++ src/main/java/guru/learningjournal/examples/kafka/jsonposfanout/services/HadoopRecordProcessorService.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package guru.learningjournal.examples.kafka.jsonposfanout.services;
+
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Service;
+
+import guru.learningjournal.examples.kafka.jsonposfanout.bindings.PosListenerBinding;
+import guru.learningjournal.examples.kafka.jsonposfanout.model.PosInvoice;
+import guru.learningjournal.examples.kafka.model.HadoopRecord;
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * 
+ */
+@Service
+@Log4j2
+@EnableBinding(PosListenerBinding.class)
+public class HadoopRecordProcessorService {
+
+	@Autowired
+	private RecordBuilder recordBuilder;
+
+	@StreamListener("hadoop-input-channel")
+	@SendTo("hadoop-output-channel")
+	public KStream<String, HadoopRecord> process(KStream<String, PosInvoice> input) {
+		KStream<String, HadoopRecord> hadoopRecordKStream = input.mapValues(v -> this.recordBuilder.getMaskedInvoice(v))
+				.flatMapValues(v -> this.recordBuilder.getHadoopRecords(v));
+		hadoopRecordKStream.foreach((k, v) -> log.info(String.format("Hadoop Record:- Key: %s, Value: %s", k, v)));
+
+		return hadoopRecordKStream;
+	}
+
+}
